fcc02ebbd56cf09ff77bf0a19e21d17ac62b9f4b
exec <exec@dragonbyte.de>
2022-04-25 00:37:52 +0200

    Added all known or presumed monster spawns

---
+++ system/scripts/monsters/morocc.cs
@@ -0,0 +1,66 @@
+﻿//--- Sabine Script ---------------------------------------------------------
+// Morocc Monster Spawns
+//--- Description -----------------------------------------------------------
+// Sets up monster spawners in and around Morocc.
+//---------------------------------------------------------------------------
+
+using System;
+using Sabine.Zone.Scripting;
+using static Sabine.Zone.Scripting.Shortcuts;
+
+public class MoroccMonstersScript : GeneralScript
+{
+	public override void Load()
+	{
+		// Morocc North-East Field
+		AddSpawner("moc_fild04", "Condor", 1009, 30);
+		AddSpawner("moc_fild04", "Steel Chocho", 1042, 20);
+		AddSpawner("moc_fild04", "PecoPeco", 1019, 10);
+		AddSpawner("moc_fild04", "Wolf", 1013, 10);
+
+		// Morocc South Field
+		AddSpawner("moc_fild01", "Condor", 1009, 30);
+		AddSpawner("moc_fild01", "Steel Chocho", 1042, 20);
+		AddSpawner("moc_fild01", "PecoPeco", 1019, 10);
+		AddSpawner("moc_fild01", "Wolf", 1013, 10);
+
+		// Morocc East Field
+		AddSpawner("moc_fild03", "Spore", 1014, 30);
+		AddSpawner("moc_fild03", "PecoPeco", 1019, 20);
+		AddSpawner("moc_fild03", "Mandragora", 1020, 10);
+		AddSpawner("moc_fild03", "Warmtail", 1024, 5);
+		AddSpawner("moc_fild03", "Boa", 1025, 10);
+
+		// Morocc North Field
+		AddSpawner("moc_fild02", "Condor", 1009, 30);
+		AddSpawner("moc_fild02", "Steel Chocho", 1042, 20);
+		AddSpawner("moc_fild02", "Wolf", 1013, 15);
+		AddSpawner("moc_fild02", "PecoPeco", 1019, 15);
+		AddSpawner("moc_fild02", "Scorpion", 1001, 10);
+
+		// Morocc Jungle
+		AddSpawner("dungeon000", "Fabre", 1007, 20);
+		AddSpawner("dungeon000", "Scorpion", 1001, 15);
+		AddSpawner("dungeon000", "Poporing", 1031, 15);
+		AddSpawner("dungeon000", "Creamy", 82, 10);
+		AddSpawner("dungeon000", "Anacondaq", 1030, 10);
+
+		// Inside Morocc Jungle
+		AddSpawner("dungeon001", "Elder Willow", 1033, 30);
+		AddSpawner("dungeon000", "Poporing", 1031, 20);
+		AddSpawner("dungeon000", "Anacondaq", 1030, 20);
+		AddSpawner("dungeon001", "Doppelganger", 1046, 1, initialDelay: TimeSpan.FromHours(3), respawnDelay: TimeSpan.FromHours(6));
+
+		// Pyramid 1F
+		AddSpawner("moc_dugn01", "Farmiliar", 1005, 20);
+		AddSpawner("moc_dugn01", "Skel Archer", 1016, 15);
+		AddSpawner("moc_dugn01", "Skel Soldier", 1028, 15);
+		AddSpawner("moc_dugn01", "Munak", 1026, 15);
+		AddSpawner("moc_dugn01", "Isis", 1029, 10);
+
+		// Pyramid 2F
+		AddSpawner("moc_dugn02", "Mummy", 105, 20);
+		AddSpawner("moc_dugn02", "Vorit", 1032, 20);
+		AddSpawner("moc_dugn02", "Osiris", 1046, 1, initialDelay: TimeSpan.FromHours(3), respawnDelay: TimeSpan.FromHours(6));
+	}
+}
---
+++ system/scripts/monsters/prontera.cs
@@ -0,0 +1,61 @@
+﻿//--- Sabine Script ---------------------------------------------------------
+// Prontera Monster Spawns
+//--- Description -----------------------------------------------------------
+// Sets up monster spawners in and around Prontera.
+//---------------------------------------------------------------------------
+
+using System;
+using Sabine.Zone.Scripting;
+using static Sabine.Zone.Scripting.Shortcuts;
+
+public class PronteraMonstersScript : GeneralScript
+{
+	public override void Load()
+	{
+		// Prontera North Field
+		AddSpawner("prt_fild01", "Poring", 1002, 30);
+		AddSpawner("prt_fild01", "Fabre", 1007, 20);
+		AddSpawner("prt_fild01", "Chocho", 1011, 10);
+
+		// Prontera West Field
+		AddSpawner("prt_fild05", "Poring", 1002, 30);
+		AddSpawner("prt_fild05", "Fabre", 1007, 20);
+		AddSpawner("prt_fild05", "Chocho", 1011, 10);
+		AddSpawner("prt_fild05", "Wilow", 1010, 5);
+		AddSpawner("prt_fild05", "Spore", 1014, 5);
+		AddSpawner("prt_fild05", "Roda Frog", 1012, 3);
+
+		// Prontera East Field
+		AddSpawner("prt_fild04", "Poring", 1002, 30);
+		AddSpawner("prt_fild04", "Fabre", 1007, 20);
+		AddSpawner("prt_fild04", "Chocho", 1011, 10);
+		AddSpawner("prt_fild04", "Roda Frog", 1012, 3);
+
+		// Prontera South Field
+		AddSpawner("prt_fild00", "Poring", 1002, 30);
+		AddSpawner("prt_fild00", "Fabre", 1007, 20);
+		AddSpawner("prt_fild00", "Chocho", 1011, 10);
+
+		// Underground Waterway
+		AddSpawner("prt_dugn00", "Poring", 1002, 30);
+		AddSpawner("prt_dugn00", "Farmiliar", 1005, 20);
+
+		// Prontera Dungeon 1B
+		AddSpawner("prt_dugn01", "Poring", 1002, 30);
+		AddSpawner("prt_dugn01", "Spore", 1014, 15);
+		AddSpawner("prt_dugn01", "Willow", 1010, 15);
+		AddSpawner("prt_dugn01", "Skel Archer", 1016, 10);
+		AddSpawner("prt_dugn01", "Skel Soldier", 1028, 10);
+
+		// Prontera Dungeon 2B
+		AddSpawner("prt_dugn02", "Spore", 1014, 30);
+		AddSpawner("prt_dugn02", "Elder Willow", 1033, 20);
+		AddSpawner("prt_dugn02", "Poporing", 1031, 20);
+		AddSpawner("prt_dugn02", "Skel Soldier", 1028, 10);
+
+		// Prontera Dungeon 3B
+		AddSpawner("prt_dugn03", "Willow", 1010, 30);
+		AddSpawner("prt_dugn03", "Elder Willow", 1033, 20);
+		AddSpawner("prt_dugn03", "Seahorse", 1043, 15);
+	}
+}
