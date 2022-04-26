6c686fe49f5cb448fa3169093a8dc755a2a8d898
exec <exec@dragonbyte.de>
2022-04-26 12:46:50 +0200

    Completed op list for Beta1

--- src/Shared/Network/PacketTable.cs
+++ src/Shared/Network/PacketTable.cs
@@ -52,10 +52,11 @@ namespace Sabine.Shared.Network
 			Register(Op.ZC_STOPMOVE, 0x0024, 10);
 			Register(Op.CZ_REQUEST_ACT, 0x0025, 7);
 			Register(Op.ZC_NOTIFY_ACT, 0x0026, 27);
-			Register(Op.ZC_NOTIFY_ACT_POSITION, 0x0027, 23); // Beta+
+			Register(Op.ZC_NOTIFY_ACT_POSITION, 0x0027, 23); // Added in Beta1
 			Register(Op.CZ_REQUEST_CHAT, 0x0028, -1);
 			Register(Op.ZC_NOTIFY_CHAT, 0x0029, -1);
 			Register(Op.ZC_NOTIFY_PLAYERCHAT, 0x002A, -1);
+			//Register(Op.SERVER_ENTRY_ACK, 0x002B, ?); // Not in client's packet table
 			Register(Op.CZ_CONTACTNPC, 0x002C, 7);
 			Register(Op.ZC_NPCACK_MAPMOVE, 0x002D, 22);
 			Register(Op.ZC_NPCACK_SERVERMOVE, 0x002E, 28);
@@ -188,53 +189,54 @@ namespace Sabine.Shared.Network
 			Register(Op.ZC_ADD_SKILL, 0x00AD, 39);
 
 			// Added in Beta
-			Register(0x00AE, 4);
-			Register(0x00AF, 10);
-			Register(0x00B0, 31);
-			Register(0x00B1, 35);
-			Register(0x00B2, 10);
-			Register(0x00B3, 18);
-			Register(0x00B4, 2);
-			Register(0x00B5, 13);
-			Register(0x00B6, 15);
-			Register(0x00B7, 20);
-			Register(0x00B8, 52);
-			Register(0x00B9, 2);
-			Register(0x00BA, 3);
-			Register(0x00BB, 11);
-			Register(0x00BC, 6);
-			Register(0x00BD, 14);
-			Register(0x00BE, -1);
-			Register(0x00BF, -1);
-			Register(0x00C0, 32);
-			Register(0x00C1, 8);
-			Register(0x00C2, 8);
-			Register(0x00C3, 8);
-			Register(0x00C4, 8);
-			Register(0x00C5, 8);
-			Register(0x00C6, 2);
-			Register(0x00C7, 2);
-			Register(0x00C8, 3);
-			Register(0x00C9, 4);
-			Register(0x00CA, 2);
-			Register(0x00CB, -1);
-			Register(0x00CC, 6);
-			Register(0x00CD, 86);
-			Register(0x00CE, 6);
-			Register(0x00CF, -1);
-			Register(0x00D0, -1);
-			Register(0x00D1, 7);
-			Register(0x00D2, -1);
-			Register(0x00D3, 6);
-			Register(0x00D4, 3);
-			Register(0x00D5, 16);
-			Register(0x00D6, 4);
-			Register(0x00D7, 4);
-			Register(0x00D8, 4);
-			Register(0x00D9, 6);
-			Register(0x00DA, 24);
-			Register(0x00DB, 26);
-			Register(0x00DC, 18);
+			Register(Op.CZ_UPGRADE_SKILLLEVEL, 0x00AE, 4);
+			Register(Op.CZ_USE_SKILL, 0x00AF, 10);
+			Register(Op.ZC_NOTIFY_SKILL, 0x00B0, 31);
+			Register(Op.ZC_NOTIFY_SKILL_POSITION, 0x00B1, 35);
+			Register(Op.CZ_USE_SKILL_TOGROUND, 0x00B2, 10);
+			Register(Op.ZC_NOTIFY_GROUNDSKILL, 0x00B3, 18);
+			Register(Op.CZ_CANCEL_LOCKON, 0x00B4, 2);
+			Register(Op.ZC_STATE_CHANGE, 0x00B5, 13);
+			Register(Op.ZC_USE_SKILL, 0x00B6, 15);
+			Register(Op.CZ_SELECT_WARPPOINT, 0x00B7, 20);
+			Register(Op.ZC_WARPLIST, 0x00B8, 52);
+			Register(Op.CZ_REMEMBER_WARPPOINT, 0x00B9, 2);
+			Register(Op.ZC_ACK_REMEMBER_WARPPOINT, 0x00BA, 3);
+			Register(Op.ZC_SKILL_ENTRY, 0x00BB, 11);
+			Register(Op.ZC_SKILL_DISAPPEAR, 0x00BC, 6);
+			Register(Op.ZC_NOTIFY_CARTITEM_COUNTINFO, 0x00BD, 14);
+			Register(Op.ZC_CART_EQUIPMENT_ITEMLIST, 0x00BE, -1);
+			Register(Op.ZC_CART_NORMAL_ITEMLIST, 0x00BF, -1);
+			Register(Op.ZC_ADD_ITEM_TO_CART, 0x00C0, 32);
+			Register(Op.ZC_DELETE_ITEM_FROM_CART, 0x00C1, 8);
+			Register(Op.CZ_MOVE_ITEM_FROM_BODY_TO_CART, 0x00C2, 8);
+			Register(Op.CZ_MOVE_ITEM_FROM_CART_TO_BODY, 0x00C3, 8);
+			Register(Op.CZ_MOVE_ITEM_FROM_STORE_TO_CART, 0x00C4, 8);
+			Register(Op.CZ_MOVE_ITEM_FROM_CART_TO_STORE, 0x00C5, 8);
+			Register(Op.CZ_REQ_CARTOFF, 0x00C6, 2);
+			Register(Op.ZC_CARTOFF, 0x00C7, 2);
+			Register(Op.ZC_ACK_ADDITEM_TO_CART, 0x00C8, 3);
+			Register(Op.ZC_OPENSTORE, 0x00C9, 4);
+			Register(Op.CZ_REQ_CLOSESTORE, 0x00CA, 2);
+			Register(Op.CZ_REQ_OPENSTORE, 0x00CB, -1);
+			Register(Op.CZ_REQ_BUY_FROMMC, 0x00CC, 6);
+			Register(Op.ZC_STORE_ENTRY, 0x00CD, 86);
+			Register(Op.ZC_DISAPPEAR_ENTRY, 0x00CE, 6);
+			Register(Op.ZC_PC_PURCHASE_ITEMLIST_FROMMC, 0x00CF, -1);
+			Register(Op.CZ_PC_PURCHASE_ITEMLIST_FROMMC, 0x00D0, -1);
+			Register(Op.ZC_PC_PURCHASE_RESULT_FROMMC, 0x00D1, 7);
+			Register(Op.ZC_PC_PURCHASE_MYITEMLIST, 0x00D2, -1);
+			Register(Op.ZC_DELETEITEM_FROM_MCSTORE, 0x00D3, 6);
+			Register(Op.CZ_PKMODE_CHANGE, 0x00D4, 3);
+			Register(Op.ZC_ATTACK_FAILURE_FOR_DISTANCE, 0x00D5, 16);
+			Register(Op.ZC_ATTACK_RANGE, 0x00D6, 4);
+			Register(Op.ZC_ACTION_FAILURE, 0x00D7, 4);
+			Register(Op.ZC_EQUIP_ARROW, 0x00D8, 4);
+			Register(Op.ZC_RECOVERY, 0x00D9, 6);
+			Register(Op.ZC_USESKILL_ACK, 0x00DA, 24);
+			Register(Op.CZ_ITEM_CREATE, 0x00DB, 26);
+			Register(Op.CZ_MOVETO_MAP, 0x00DC, 18); // ?
+			///Register(Op.ZC_COUPLESTATUS,0x00DD, 14); // There's a handler for it, but it doesn't appear in the packet table
 
 			RegisterNames();
 		}
