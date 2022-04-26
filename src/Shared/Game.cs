ad2f4e00cbe3a19c86f199af053503a035885b17
exec <exec@dragonbyte.de>
2022-04-26 18:30:53 +0200

    Minor change

--- src/Shared/Game.cs
+++ src/Shared/Game.cs
@@ -1,14 +1,35 @@
 ﻿namespace Sabine.Shared
 {
+	/// <summary>
+	/// Global accessor for the application settings.
+	/// </summary>
 	public static class Game
 	{
+		/// <summary>
+		/// The packet version to use.
+		/// </summary>
 		public static int Version = 2;
 	}
 
+	/// <summary>
+	/// A list of known packet versions.
+	/// </summary>
 	public static class Versions
 	{
+		/// <summary>
+		/// The packet version for the iRO Alpha client (2001-08-30)
+		/// </summary>
 		public const int Alpha = 0;
+
+		/// <summary>
+		/// The packet version for the iRO Beta 1 client (2002-02-20)
+		/// </summary>
 		public const int Beta1 = 2;
+
+		/// <summary>
+		/// The packet version for the jRO Beta 2 client (2002-08-09)
+		/// </summary>
+		public const int Beta2 = 3;
 	}
 
 	/// <summary>
