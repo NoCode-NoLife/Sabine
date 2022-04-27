b7c5764e48e3efad5c3d183fc1879b96c73a1552
exec <exec@dragonbyte.de>
2022-04-27 16:01:22 +0200

    Updated ZC_EQUIPMENT_ITEMLIST for EP5

--- src/ZoneServer/Network/Send.cs
+++ src/ZoneServer/Network/Send.cs
@@ -789,31 +789,31 @@ namespace Sabine.Zone.Network
 
 				if (Game.Version < Versions.Beta2)
 				{
-				var size = 6 + Sizes.ItemNames;
-				if (Game.Version >= Versions.Beta1)
-					size += 4;
+					var size = 6 + Sizes.ItemNames;
+					if (Game.Version >= Versions.Beta1)
+						size += 4;
 
-				// The first byte contains the size of the item
-				// struct plus the size byte, which the client
-				// memcpys for handling. It's currently unclear
-				// why this size byte is necessary, but it's
-				// working this way.
-				packet.PutByte((byte)size);
+					// The first byte contains the size of the item
+					// struct plus the size byte, which the client
+					// memcpys for handling. It's currently unclear
+					// why this size byte is necessary, but it's
+					// working this way.
+					packet.PutByte((byte)size);
 
-				packet.PutByte((byte)item.Type);
-				packet.PutShort((short)item.InventoryId);
-				packet.PutShort((short)item.Amount);
+					packet.PutByte((byte)item.Type);
+					packet.PutShort((short)item.InventoryId);
+					packet.PutShort((short)item.Amount);
 
-				if (Game.Version >= Versions.Beta1)
-				{
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
+					if (Game.Version >= Versions.Beta1)
+					{
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+					}
+
+					packet.PutString(item.StringId, Sizes.ItemNames);
 				}
-
-				packet.PutString(item.StringId, Sizes.ItemNames);
-			}
 				else
 				{
 					packet.PutShort((short)item.InventoryId);
@@ -934,43 +934,61 @@ namespace Sabine.Zone.Network
 				if (!character.CanEquip(item))
 					wearSlots = EquipSlots.None;
 
-				var size = 6 + Sizes.ItemNames;
-				if (Game.Version >= Versions.Beta1)
-					size += 16;
-
-				// The first byte contains the size of the item
-				// struct plus the size byte, which the client
-				// memcpys for handling. It's currently unclear
-				// why this size byte is necessary, but it's
-				// working this way.
-				packet.PutByte((byte)size);
-
-				packet.PutByte((byte)item.Type);
-				packet.PutByte((byte)wearSlots);
-				packet.PutShort((short)item.InventoryId);
-				packet.PutByte((byte)item.EquippedOn);
-
-				if (Game.Version >= Versions.Beta1)
+				if (Game.Version < Versions.Beta2)
 				{
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-					packet.PutByte(0);
-				}
+					var size = 6 + Sizes.ItemNames;
+					if (Game.Version >= Versions.Beta1)
+						size += 16;
 
-				packet.PutString(item.StringId, Sizes.ItemNames);
+					// The first byte contains the size of the item
+					// struct plus the size byte, which the client
+					// memcpys for handling. It's currently unclear
+					// why this size byte is necessary, but it's
+					// working this way.
+					packet.PutByte((byte)size);
+
+					packet.PutByte((byte)item.Type);
+					packet.PutByte((byte)wearSlots);
+					packet.PutShort((short)item.InventoryId);
+					packet.PutByte((byte)item.EquippedOn);
+
+					if (Game.Version >= Versions.Beta1)
+					{
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+						packet.PutByte(0);
+					}
+
+					packet.PutString(item.StringId, Sizes.ItemNames);
+				}
+				else
+				{
+					packet.PutShort((short)item.InventoryId);
+					packet.PutShort((short)item.ClassId);
+					packet.PutByte((byte)item.Type);
+					packet.PutByte(item.IsIdentified);
+					packet.PutShort((short)wearSlots);
+					packet.PutShort((short)item.EquippedOn);
+					packet.PutByte(0);   // Attribute
+					packet.PutByte(0);   // Refine
+					packet.PutShort(0);  // Card1
+					packet.PutShort(0);  // Card2
+					packet.PutShort(0);  // Card3
+					packet.PutShort(0);  // Card4
+				}
 			}
 
 			character.Connection.Send(packet);
