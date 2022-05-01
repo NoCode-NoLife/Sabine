a811cd7b02bd3b834e33f188356c02a4bd4b2f6f
exec <exec@dragonbyte.de>
2022-05-01 18:45:09 +0200

    Minor change

--- src/ZoneServer/Commands/ConsoleCommands.cs
+++ src/ZoneServer/Commands/ConsoleCommands.cs
@@ -18,8 +18,8 @@ namespace Sabine.Zone.Commands
 		{
 			base.HandleStatus(command, args);
 
-			Log.Info("Updates Per Second: {0:0.0}", ZoneServer.Instance.World.Heartbeat.UpdatesPerSecond);
-			Log.Info("Average Update Time: {0:0.0}ms", ZoneServer.Instance.World.Heartbeat.AverageUpdateTime.TotalMilliseconds);
+			Log.Info("Updates Per Second: {0:0}", ZoneServer.Instance.World.Heartbeat.UpdatesPerSecond);
+			Log.Info("Average Update Time: {0:0.000}ms", ZoneServer.Instance.World.Heartbeat.AverageUpdateTime.TotalMilliseconds);
 			Log.Info("Players in World: {0}", ZoneServer.Instance.World.GetPlayerCount());
 
 			return CommandResult.Okay;
