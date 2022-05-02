62f86e6730733c6dc5cfd8746594fd11a1218937
exec <exec@dragonbyte.de>
2022-05-02 10:53:41 +0200

    Added test NPC to Prontera

--- system/scripts/custom/test.cs
+++ system/scripts/custom/test.cs
@@ -4,7 +4,9 @@
 // Playground.
 //---------------------------------------------------------------------------
 
+using System.Threading.Tasks;
 using Sabine.Zone.Scripting;
+using Sabine.Zone.Scripting.Dialogues;
 using Sabine.Zone.World.Shops;
 using static Sabine.Zone.Scripting.Shortcuts;
 
@@ -12,83 +14,85 @@ public class TestNpcScript : GeneralScript
 {
 	public override void Load()
 	{
-		if (!MapsExist("prt_vilg02"))
-			return;
-
 		AddShop("TestShop1", shop =>
 		{
 			shop.AddItem(ItemId.RedPotion, 100);
 			shop.AddItem(ItemId.Sword, 200);
 		});
 
-		AddNpc("Test", 54, "prt_vilg02", 105, 89, async dialog =>
+		if (MapsExist("prt_vilg02"))
+			AddNpc("Test", 54, "prt_vilg02", 105, 89, TestDialog);
+		else if (MapsExist("prontera"))
+			AddNpc("Test", 54, "prontera", 157, 193, TestDialog);
+	}
+
+	private static async Task TestDialog(Dialog dialog)
+	{
+		dialog.PlayerLocalization(out var L, out var LN);
+
+		dialog.Msg("[Test]");
+		dialog.Msg("Hello, world!");
+		await dialog.Next();
+
+		dialog.Msg("[Test]");
+		dialog.Msg(L("How are you?"));
+		await dialog.Next();
+
+		var response = await dialog.Select(Option("Good!", "good"), Option("Meh.", "bad"));
+
+		dialog.Msg("[Test]");
+		switch (response)
 		{
-			dialog.PlayerLocalization(out var L, out var LN);
-
-			dialog.Msg("[Test]");
-			dialog.Msg("Hello, world!");
-			await dialog.Next();
-
-			dialog.Msg("[Test]");
-			dialog.Msg(L("How are you?"));
-			await dialog.Next();
-
-			var response = await dialog.Select(Option("Good!", "good"), Option("Meh.", "bad"));
-
-			dialog.Msg("[Test]");
-			switch (response)
+			case "good":
 			{
-				case "good":
-				{
-					dialog.Msg("That's nice to hear! Sounds like you're in the mood for a good deal!");
-					await dialog.Next();
-					dialog.OpenShop(NpcShop.Build(501, -1, 502, -1, 503, 250), ShopOpenType.BuyOnly);
-					break;
-				}
-				case "bad":
-				{
-					dialog.Msg("Aw, you should buy yourself something nice then! That will cheer you right up!");
-					await dialog.Next();
-					dialog.OpenShop("TestShop1");
-					break;
-				}
+				dialog.Msg("That's nice to hear! Sounds like you're in the mood for a good deal!");
+				await dialog.Next();
+				dialog.OpenShop(NpcShop.Build(501, -1, 502, -1, 503, 250), ShopOpenType.BuyOnly);
+				break;
 			}
+			case "bad":
+			{
+				dialog.Msg("Aw, you should buy yourself something nice then! That will cheer you right up!");
+				await dialog.Next();
+				dialog.OpenShop("TestShop1");
+				break;
+			}
+		}
 
-			// Move along, just having fun here.
+		// Move along, just having fun here.
 
-			//dialog.AthenaFTW(out var mes, out var next, out var select);
+		//dialog.AthenaFTW(out var mes, out var next, out var select);
 
-			//mes("[Test]");
-			//mes("Hello, world!");
-			//await next();
+		//mes("[Test]");
+		//mes("Hello, world!");
+		//await next();
 
-			//mes("[Test]");
-			//mes("How are you?");
-			//await next();
+		//mes("[Test]");
+		//mes("How are you?");
+		//await next();
 
-			//var response = await select("Good!", "Meh");
+		//var response = await select("Good!", "Meh");
 
-			//mes("[Test]");
-			//switch (response)
-			//{
-			//	case 1: mes("That's nice to hear!"); break;
-			//	case 2: mes("Toughen up!"); break;
-			//}
+		//mes("[Test]");
+		//switch (response)
+		//{
+		//	case 1: mes("That's nice to hear!"); break;
+		//	case 2: mes("Toughen up!"); break;
+		//}
 
-			// Style test for what NPCs could look like. Msg would always
-			// send a wait at the end and codes like p and br could be
-			// used to split up the message. The NPC's name would become
-			// the default title and would always be displayed with each
-			// message, unless unset.
+		// Style test for what NPCs could look like. Msg would always
+		// send a wait at the end and codes like p and br could be
+		// used to split up the message. The NPC's name would become
+		// the default title and would always be displayed with each
+		// message, unless unset.
 
-			//await dialog.MsgAdv("Hello, world!<p/>How are you?");
-			//var response = await dialog.Select(Option("Good!", "good"), Option("Meh.", "bad"));
+		//await dialog.MsgAdv("Hello, world!<p/>How are you?");
+		//var response = await dialog.Select(Option("Good!", "good"), Option("Meh.", "bad"));
 
-			//switch (response)
-			//{
-			//	case "good": await dialog.MsgAdv("That's nice to hear!"); break;
-			//	case "bad": await dialog.MsgAdv("Toughen up!"); break;
-			//}
-		});
+		//switch (response)
+		//{
+		//	case "good": await dialog.MsgAdv("That's nice to hear!"); break;
+		//	case "bad": await dialog.MsgAdv("Toughen up!"); break;
+		//}
 	}
 }
