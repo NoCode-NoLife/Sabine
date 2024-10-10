4b821f58e7873ddfc81fc27fa2c08efc2a115525
Exec Tails <exec@dragonbyte.de>
2024-10-10 13:30:24 +0200

    Added character initialization script

---
+++ system/scripts/other/character_init.cs
@@ -0,0 +1,24 @@
+﻿//--- Melia Script ----------------------------------------------------------
+// Character Initialization
+//--- Description -----------------------------------------------------------
+// Grants default items to newly created characters.
+//---------------------------------------------------------------------------
+
+using Sabine.Shared.Scripting;
+using Sabine.Zone.Events.Args;
+using Sabine.Zone.Scripting;
+
+public class CharacterInitializationScript : GeneralScript
+{
+	[On("PlayerReady")]
+	public void OnPlayerReady(object sender, PlayerEventArgs args)
+	{
+		var character = args.Character;
+
+		if (!character.Vars.Perm.ActivateOnce("Sabine.Initialized"))
+			return;
+
+		character.Inventory.Add(ItemId.Knife, 1);
+		character.Inventory.Add(ItemId.CottonShirt, 1);
+	}
+}
