dedfcd141bd7eb91cf87da96925244d4f72b0202
Exec Tails <exec@dragonbyte.de>
2026-04-23 00:45:55 +0200

    Updated EmotionId enum

--- src/Shared/Const/EmotionId.cs
+++ src/Shared/Const/EmotionId.cs
@@ -5,6 +5,9 @@
 	/// </summary>
 	public enum EmotionId : byte
 	{
+		// Alpha
+		//-------------------------------------------------------------------
+
 		/// <summary>
 		/// /!
 		/// </summary>
@@ -16,12 +19,12 @@
 		QuestionMark = 1,
 
 		/// <summary>
-		/// ho
+		/// /ho
 		/// </summary>
 		MusicNote = 2,
 
 		/// <summary>
-		/// lv
+		/// /lv
 		/// </summary>
 		BeatingHeart = 3,
 
@@ -69,5 +72,118 @@
 		/// /bo
 		/// </summary>
 		Paper = 12,
+
+		// EP3_2 (iRO 2003-10-31)
+		//-------------------------------------------------------------------
+
+		/// <summary>
+		/// ?
+		/// </summary>
+		KoreanFlag = 13,
+
+		/// <summary>
+		/// /lv2
+		/// </summary>
+		BigHeart = 14,
+
+		/// <summary>
+		/// /thx
+		/// </summary>
+		Thanks = 15,
+
+		/// <summary>
+		/// /wah
+		/// </summary>
+		Panic = 16,
+
+		/// <summary>
+		/// /sry
+		/// </summary>
+		Sorry = 17,
+
+		/// <summary>
+		/// /heh
+		/// </summary>
+		Laughing = 18,
+
+		/// <summary>
+		/// /swt2
+		/// </summary>
+		Sweat2 = 19,
+
+		/// <summary>
+		/// /hmm
+		/// </summary>
+		Thinking = 20,
+
+		/// <summary>
+		/// /no1
+		/// </summary>
+		ThumbsUp = 21,
+
+		/// <summary>
+		/// /??
+		/// </summary>
+		ShakingHead = 22,
+
+		/// <summary>
+		/// /omg
+		/// </summary>
+		Shocked = 23,
+
+		/// <summary>
+		/// /oh
+		/// </summary>
+		YesSign = 24,
+
+		/// <summary>
+		/// /X
+		/// </summary>
+		NoSign = 25,
+
+		/// <summary>
+		/// ?
+		/// </summary>
+		HelpCry = 26,
+
+		/// <summary>
+		/// ?
+		/// </summary>
+		Go = 27,
+
+		/// <summary>
+		/// /sob
+		/// </summary>
+		Sobbing = 28,
+
+		/// <summary>
+		/// /gg
+		/// </summary>
+		EvilGiggle = 29,
+
+		/// <summary>
+		/// /kis
+		/// </summary>
+		KissRight = 30,
+
+		/// <summary>
+		/// /kis2
+		/// </summary>
+		KissLeft = 31,
+
+		/// <summary>
+		/// /pif
+		/// </summary>
+		Pif = 32,
+
+		/// <summary>
+		/// /ok
+		/// </summary>
+		Nod = 33,
+
+		/// <summary>
+		/// ?
+		/// </summary>
+		Muted = 34,
 	}
 }
