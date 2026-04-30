d38d4f94fb264b8cbd6a51c39da82177d6999f42
Exec Tails <exec@dragonbyte.de>
2026-04-30 23:26:15 +0200

    Added test handler for Firebolt

---
+++ src/ZoneServer/Skills/Handlers/Mage/MG_FIREBOLT.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Sabine.Shared;
+using Sabine.Shared.Const;
+using Sabine.Zone.Network;
+
+namespace Sabine.Zone.Skills.Handlers.Mage
+{
+	/// <summary>
+	/// Handler for the Mage skill "Firebolt".
+	/// </summary>
+	[SkillHandler(SkillId.MG_FIREBOLT)]
+	public class MG_FIREBOLT : ITargetedSkillHandler
+	{
+		/// <summary>
+		/// Handles the skill.
+		/// </summary>
+		/// <param name="parameters"></param>
+		public void Handle(UseSkillParams parameters)
+		{
+			var attacker = parameters.Character;
+			var target = parameters.Target;
+			var skill = parameters.Skill;
+			var skillLevel = parameters.SkillLevel;
+
+			if (!attacker.TrySpendSp(skill.SpCost))
+			{
+				Send.ZC_ACK_TOUSESKILL(attacker, skill.Id, SkillUseResult.Fail, SkillFailType.NotEnoughSp, SkillFailReason.SkillFailed);
+				return;
+			}
+
+			attacker.Controller.StopMove();
+			target.Controller.StopMove();
+
+			var hitCount = skillLevel;
+
+			var damage = Random.Shared.Next(attacker.Parameters.MagicAttackMin, attacker.Parameters.MagicAttackMax + 1);
+			damage *= hitCount;
+
+			target.TakeDamage(damage, attacker);
+			target.StunEndTime = DateTime.Now.AddSeconds(1);
+
+			Send.ZC_NOTIFY_SKILL(attacker, target, skill, skillLevel, damage, Game.GetTick(), attacker.Parameters.AttackMotionDelay, target.Parameters.DamageMotionDelay, hitCount, SkillHitEffect.MultiHit);
+		}
+	}
+}
